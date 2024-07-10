065affe9c094cb9a03e3ed1c3775a52f743dac0b
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-07-10 22:37:29 +0300

    remove archived rest.nvim && some minor ui stuff

--- lua/custom/plugins/extra.lua
+++ lua/custom/plugins/extra.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1000,
-    config = true,
-    opts = {
-      rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
-    },
-  },
-  {
-    'rest-nvim/rest.nvim',
-    ft = 'http',
-    dependencies = { 'luarocks.nvim' },
-    config = function()
-      require('rest-nvim').setup()
-      -- first load extension
-      require('telescope').load_extension 'rest'
-      -- then use it, you can also use the `:Telescope rest select_env` command
-      -- require('telescope').extensions.rest.select_env()
-    end,
-  },
+  -- {
+  --   'vhyrro/luarocks.nvim',
+  --   priority = 1000,
+  --   config = true,
+  --   opts = {
+  --     rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
+  --   },
+  -- },
+  -- {
+  --   'rest-nvim/rest.nvim',
+  --   ft = 'http',
+  --   dependencies = { 'luarocks.nvim' },
+  --   config = function()
+  --     require('rest-nvim').setup()
+  --     -- first load extension
+  --     require('telescope').load_extension 'rest'
+  --     -- then use it, you can also use the `:Telescope rest select_env` command
+  --     -- require('telescope').extensions.rest.select_env()
+  --   end,
+  -- },
 }
--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -1,3 +1,31 @@
+-- local colors = {
+--   blue   = '#80a0ff',
+--   cyan   = '#79dac8',
+--   black  = '#080808',
+--   white  = '#c6c6c6',
+--   red    = '#ff5189',
+--   violet = '#d183e8',
+--   grey   = '#303030',
+-- }
+
+-- local bubbles_theme = {
+--   normal = {
+--     a = { fg = colors.black, bg = colors.violet },
+--     b = { fg = colors.white, bg = colors.grey },
+--     c = { fg = colors.white },
+--   },
+--
+--   insert = { a = { fg = colors.black, bg = colors.blue } },
+--   visual = { a = { fg = colors.black, bg = colors.cyan } },
+--   replace = { a = { fg = colors.black, bg = colors.red } },
+--
+--   inactive = {
+--     a = { fg = colors.white, bg = colors.black },
+--     b = { fg = colors.white, bg = colors.black },
+--     c = { fg = colors.white },
+--   },
+-- }
+
 return {
   {
     'folke/zen-mode.nvim',
@@ -13,4 +41,49 @@ return {
   },
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   { 'kvrohit/rasmus.nvim' },
+  { 'norcalli/nvim-colorizer.lua' },
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        -- theme = bubbles_theme,
+        theme = 'catppuccin',
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = {
+          '%=', --[[ add your center compoentnts here in place of this comment ]]
+        },
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+          { 'location', separator = { right = '' }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      tabline = {},
+      extensions = {},
+    },
+  },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        -- config
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+  { 'romgrk/barbar.nvim' },
 }
