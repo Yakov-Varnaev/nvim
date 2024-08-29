b0b38549e718786c9fcd9d3519bb4344b5ccaa28
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-08-29 17:00:08 +0300

    cleanup

--- lua/custom/plugins/extra.lua
+++ lua/custom/plugins/extra.lua
@@ -1,14 +1,5 @@
 return {
   {
     'rest-nvim/rest.nvim',
-    ft = 'http',
-    dependencies = { 'luarocks.nvim' },
-    config = function()
-      require('rest-nvim').setup()
-      -- first load extension
-      require('telescope').load_extension 'rest'
-      -- then use it, you can also use the `:Telescope rest select_env` command
-      -- require('telescope').extensions.rest.select_env()
-    end,
   },
 }
